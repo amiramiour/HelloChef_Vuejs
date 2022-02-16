70217cdac267319770bc319e2e008f76d5bd0854
Nicolas T <nicolas.trugeon@univ-lr.fr>
2022-02-16 15:37:06 +0100

    Projet Vide

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import ARenommer from './components/ARenommer.vue'
+</script>
+
+<template>
+  <main>
+    <ARenommer />
+  </main>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/ARenommer.vue
@@ -0,0 +1,10 @@
+<template>
+  <h2>exemple titre à renommer</h2> 
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+</style>
