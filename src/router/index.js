213bc3ef8cf240049888ca4637718f0a592ce95f
Nicolas T <nicolas.trugeon@univ-lr.fr>
2022-02-17 11:28:07 +0100

    config minimal du routeur

---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const routes = [
+    // À compléter
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+  })
+
+export default router
